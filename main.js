0f317bb8c866bb6ebef58ff6a2d36c334275b5b0
jcbackenddev <jcbackenddev@gmail.com>
2020-10-12 12:23:26 +0200

    tabs added to the index

--- main.js
+++ main.js
@@ -61,3 +61,77 @@ new Vue({
         showMessageModal: false
     }
 })
+
+Vue.component('tabs', {
+    template: `
+    
+        <div>
+            <div class="tabs">
+                <ul>
+                    <li v-for="tab in tabs" :class="{ 'is-active': tab.tabIsActive}">
+                        <a :href="tab.href" @click="selectTab(tab)">{{ tab.name }} </a>
+                    </li>
+                </ul>
+            </div>
+
+            <div class="tabs-details">
+                <slot></slot>
+            </div>
+        </div>
+
+    `,
+
+    data() {
+        return { tabs: [] };
+    },
+
+    created() {
+        this.tabs = this.$children;
+        // console.log(this.$children);
+    },
+
+    methods: {
+        selectTab(selectedTab) {
+            this.tabs.forEach(tab => {
+                tab.tabIsActive = (tab.name == selectedTab.name);
+            })
+        }
+    }
+
+});
+
+Vue.component('tab', {
+
+    template: `
+
+    <div v-show="tabIsActive">
+        <slot></slot>
+    </div>
+
+    `,
+
+    props: {
+        name: { required: true },
+        selected: { default: false }
+    },
+
+    data() {
+        return {
+            tabIsActive: false
+        };
+    },
+
+    computed: {
+        href() {
+            return '#' + this.name.toLowerCase().replace(/ /g, '-');
+        }
+    },
+
+    mounted() {
+        this.tabIsActive = this.selected;
+    },
+});
+
+new Vue ({
+    el:"#tabs-template"
+});
